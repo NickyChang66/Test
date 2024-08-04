6264192497eb9ed805d91fa9c7c3db99e45f69d7
Nicky Chang <iamexnimo@gmail.com>
2024-08-04 08:57:12 +0800

    add int test

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,13 @@
 public class Calculator {
+
+    private String  name = "Casio";
+    private int calculationCount;
+    private double results = -1.0;
+    private int test;
+
     public int add(int a, int b) {
         return a + b;
     }
-
     public int subtract(int a, int b) {
         return a - b;
     }
