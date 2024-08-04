37a821c066a3dbec38d9b3f588653eaa32c99887
Nicky Chang <iamexnimo@gmail.com>
2024-08-04 08:46:03 +0800

    add int CalCount

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,11 @@
 public class Calculator {
+
+    private String  name = "Casio";
+    private int calculationCount;
+
     public int add(int a, int b) {
         return a + b;
     }
-
     public int subtract(int a, int b) {
         return a - b;
     }
